09606d1ebe86cbf0021d01086f73877879db9d22
Amaan Sheikh <amaanomo@gmail.com>
2025-11-29 15:33:43 -0500

    Update landing page video

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -134,7 +134,7 @@ const Hero = () => {
             disablePictureInPicture
             onContextMenu={(e) => e.preventDefault()}
           >
-            <source src="/addedVids/spurhacks.mp4" type="video/mp4" />
+            <source src="/addedVids/QuickTime%20movie.mp4" type="video/mp4" />
             Your browser does not support the video tag.
           </video>
         </div>
